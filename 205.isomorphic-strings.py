73d8282bb5660173ac8d3a2cf60c50ac6c0ee169
sybil <arenvista1@gmail.com>
2024-05-20 15:37:04 -0400

    simplify path solution

--- 205.isomorphic-strings.py
+++ 205.isomorphic-strings.py
@@ -1,5 +1,10 @@
 # @leet start
+from collections import Counter
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        
+        s = (sorted  ( list(Counter(s).values()) ) )
+        t = (sorted ( list(Counter(t).values()) ) )
+        print(s)
+        print(t)
+        return s == t
 # @leet end

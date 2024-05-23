5cdec2ebe3dfbf5b8fcfeb9987955eb209d00423
sybil <arenvista1@gmail.com>
2024-05-23 14:03:03 -0400

    llc sol

--- 141.linked-list-cycle.py
+++ 141.linked-list-cycle.py
@@ -1,20 +1,19 @@
 # @leet start
 # Definition for singly-linked list.
-from typing import Optional
-
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return False
         node = head
         while node != None:
-            if node.val == 'visited':
+            if node.val == "seen":
                 return True
-            node.val = 'visited'
+            node.val = "seen"
             node = node.next
-        return False
-        
+
 # @leet end
